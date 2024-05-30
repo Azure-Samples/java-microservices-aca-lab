19525181a374bc80c76ac9ca9eae6fe5367aed1d
Sonalika Roy <83619402+sonalika-roy@users.noreply.github.com>
2024-05-30 15:30:53 -0700

    Delete src/staging-acr/createuser.sql

--- src/staging-acr/createuser.sql
+++
@@ -1,5 +0,0 @@
-SET aad_auth_validate_oids_in_tenant = OFF;
-DROP USER IF EXISTS 'mysql_conn'@'%';
-CREATE AADUSER 'mysql_conn' IDENTIFIED BY 'a57068a9-bbcb-4b9c-a7d8-fe49b6ab3f5e';
-GRANT ALL PRIVILEGES ON petclinic.* TO 'mysql_conn'@'%';
-FLUSH privileges;
