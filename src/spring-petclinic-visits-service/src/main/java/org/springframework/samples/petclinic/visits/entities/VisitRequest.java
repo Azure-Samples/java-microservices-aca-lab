08f1abed8acd65e14cf1385db301389bf6ccb1fb
Hao Zhang <haozhan@microsoft.com>
2024-09-12 11:32:09 +0800

    include message emulator, and support service bus in visits service

---
+++ src/spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/entities/VisitRequest.java
@@ -0,0 +1,38 @@
+package org.springframework.samples.petclinic.visits.entities;
+
+import java.io.Serializable;
+
+public class VisitRequest implements Serializable {
+    private static final long serialVersionUID = -249974321255677286L;
+
+    private Integer requestId;
+    private Integer petId;
+    private String message;
+
+    public VisitRequest() {
+    }
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(Integer id) {
+        this.requestId = id;
+    }
+
+    public Integer getPetId() {
+        return petId;
+    }
+
+    public void setPetId(Integer petId) {
+        this.petId = petId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
