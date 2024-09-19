02d333d9b85a835dcbfa433110bbd1f40aade3b0
Hao Zhang <haozhan@microsoft.com>
2024-09-19 16:18:26 +0800

    small fix - 0418

--- src/spring-petclinic-chat-agent/src/main/java/org/springframework/samples/petclinic/agent/ChatAgentApplication.java
+++ src/spring-petclinic-chat-agent/src/main/java/org/springframework/samples/petclinic/agent/ChatAgentApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
-/**
- * PetClinic Spring Boot Application.
- */
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class ChatAgentApplication {
