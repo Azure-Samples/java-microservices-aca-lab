d02d238aee4fcbc418865296d53f077297b6f22b
Songbo Wang <sonwan@microsoft.com>
2024-10-11 15:31:12 +0800

    support save & load environment variables for dev container (#79)
    ## Purpose
    
    save & load environment for dev container
    
    manual save only
    
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [x] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [ ] Bugfix
    [x] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DEV_ENV_FILE="\$HOME/.dev-environment"
+
+cat <<EOT >> "$HOME/.bashrc"
+
+# auto load
+if [[ -f "$DEV_ENV_FILE" ]]; then
+    source "$DEV_ENV_FILE"
+fi
+
+saveenv() {
+    # Check if var_save is set
+    declare -p | grep -v "declare -[a-z]*r" > "$DEV_ENV_FILE"
+}
+
+clearenv() {
+    echo "" > "$DEV_ENV_FILE"
+}
+
+EOT
