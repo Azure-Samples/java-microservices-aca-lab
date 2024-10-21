a18ed2ab7ffdf5ee45a5f53c7114cd3742133f17
Songbo Wang <sonwan@microsoft.com>
2024-10-21 11:24:35 +0800

    Revert "add required extension in postprovision script"
    This reverts commit da314d5d1862f8e8bc4925b571c5a00c542bb7ef.

--- infra/bicep/hooks/postprovision.sh
+++ infra/bicep/hooks/postprovision.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-set -x
-
 azd env set AZURE_RESOURCE_GROUP $resourceGroupName
 
 azd env set AZURE_CONTAINER_REGISTRY_ENDPOINT $acrLoginServer
 
 azd env set AZD_PROVISION_TIMESTAMP $azdProvisionTimestamp
-
-az extension add --upgrade -n containerapp --allow-preview true
-az extension add --upgrade -n serviceconnector-passwordless
-
-az extension list -o table
-
-azd env list
 
 # refresh service connection, via customers-service
 az containerapp connection create mysql-flexible --subscription $subscriptionId -g $resourceGroupName \
