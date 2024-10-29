976b30fc8bfb24dec4948ad485def8e72f32fbca
Songbo Wang <sonwan@microsoft.com>
2024-10-29 17:37:39 +0800

    refine

--- infra/bicep/hooks/postprovision.ps1
+++ infra/bicep/hooks/postprovision.ps1
@@ -5,17 +5,21 @@ azd env set AZURE_CONTAINER_REGISTRY_ENDPOINT $env:acrLoginServer
 azd env set AZD_PROVISION_TIMESTAMP $env:azdProvisionTimestamp
 
 Write-Host ""
-Write-Host "INFO: Updating container apps connection ..."
+Write-Host "INFO: " -ForegroundColor Green -NoNewline;
+Write-Host "Updating container apps connection ..."
 
 # refresh service connection, via customers-service
 az containerapp connection create mysql-flexible --subscription $env:subscriptionId -g $env:resourceGroupName `
   --connection $env:sqlConnectName --source-id $env:customersServiceId --target-id $env:sqlDatabaseId --client-type springBoot `
   --user-identity client-id=$env:appUserIdentityClientId subs-id=$env:subscriptionId mysql-identity-id=$env:sqlAdminIdentityId `
-  -c $env:customersServiceName -y
+  -c $env:customersServiceName -y | Out-Null
 
 Write-Host ""
-Write-Host "INFO: Deploy finish succeed!"
+Write-Host "INFO: " -ForegroundColor Green -NoNewline;
+Write-Host "Deploy finish succeed!"
 
-Write-Host "INFO: Api Gateway App url: https://$gatewayFqdn"
+Write-Host "INFO: " -ForegroundColor Green -NoNewline;
+Write-Host "Api Gateway App url: https://$env:gatewayFqdn"
 
-Write-Host "INFO: Spring Boot Admin url: https://$springbootAdminFqdn"
+Write-Host "INFO: " -ForegroundColor Green -NoNewline;
+Write-Host "Spring Boot Admin url: https://$env:springbootAdminFqdn"
