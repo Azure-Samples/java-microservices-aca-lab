bb5d350c0993fccff1d2d5c57d3cc58d032a0e78
Songbo Wang <sonwan@microsoft.com>
2024-12-02 14:37:34 +0800

    add scripts to create / update apps parallelly  (#142)
    ## Purpose
    and some docs refine
    
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [x] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [ ] Bugfix
    [x] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```

---
+++ src/build-update-apps.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DIR=/tmp
+
+build_and_deploy() {
+    APP_NAME=$1
+
+    echo "Start building and deploying app $APP_NAME with application insights agent ..."
+
+    az acr build --image spring-petclinic-$APP_NAME:$IMAGE_TAG --registry $MYACR \
+        --file spring-petclinic-$APP_NAME/ai.Dockerfile spring-petclinic-$APP_NAME \
+        > $DIR/$APP_NAME.build.log 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo "Build image for $APP_NAME failed, check $DIR/$APP_NAME.build.log for more details"
+        return 1
+    fi
+
+    az containerapp update \
+        --name $APP_NAME \
+        --resource-group $RESOURCE_GROUP \
+        --image $MYACR.azurecr.io/spring-petclinic-$APP_NAME:$IMAGE_TAG \
+        --set-env-vars APPLICATIONINSIGHTS_CONNECTION_STRING=$AI_CONNECTIONSTRING APPLICATIONINSIGHTS_CONFIGURATION_CONTENT='{"role": {"name": "'$APP_NAME'"}}' \
+        > $DIR/$APP_NAME.deploy.log 2>&1
+
+    if [[ $? -ne 0 ]]; then
+        echo "Deploy app $APP_NAME failed, check $DIR/$APP_NAME.deploy.log for more details"
+        return 2
+    fi
+
+    return 0
+}
+
+CHECK_FAIL=$DIR/aca-lab.$$
+
+for name in customers-service vets-service visits-service; do
+    build_and_deploy $name || touch $CHECK_FAIL &
+done
+
+wait < <(jobs -p)
+
+if [[ -f $CHECK_FAIL ]]; then
+    echo "Error happens on build and depploy apps, please check the logs for more details"
+    exit 1
+else
+    echo "Build and deploy apps succeed"
+    exit 0
+fi
+
---
+++ src/create-apps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DIR=/tmp
+
+INGRESS=internal
+
+create_app() {
+    APP_NAME=$1
+
+    echo "Start creating app $APP_NAME ..."
+
+    az containerapp create \
+        --name $APP_NAME \
+        --resource-group $RESOURCE_GROUP \
+        --source ./spring-petclinic-$APP_NAME \
+        --registry-server $MYACR.azurecr.io \
+        --registry-identity $USER_ID \
+        --environment $ACA_ENVIRONMENT \
+        --user-assigned $USER_ID \
+        --ingress $INGRESS \
+        --target-port 8080 \
+        --min-replicas 1 \
+        --bind $JAVA_CONFIG_COMP_NAME $JAVA_EUREKA_COMP_NAME \
+        --runtime java \
+        > $DIR/$APP_NAME.create.log 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo "Create app $APP_NAME failed, check $DIR/$APP_NAME.create.log for more details"
+        return 1
+    fi
+
+    return 0
+}
+
+CHECK_FAIL=$DIR/aca-lab.$$
+
+for name in customers-service vets-service visits-service; do
+    create_app $name || touch $CHECK_FAIL &
+done
+
+wait < <(jobs -p)
+
+if [[ -f $CHECK_FAIL ]]; then
+    echo "Error happens on create apps, please check the logs for more details"
+    exit 1
+else
+    echo "Create apps succeed"
+    exit 0
+fi
+
